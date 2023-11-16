1461d016a5a0ba9e5467c3b4f85e42255d231172
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2023-11-16 13:26:47 +0000

    Updated: Modified the codes

--- errors.c
+++ errors.c
@@ -15,7 +15,7 @@ void _eputs(char *leo_str)
 	while (leo_str[leo_i] != '\0')
 	{
 		_eputchar(leo_str[leo_i]);
-		leo_i++;
+		leo_i = leo_i + 1;
 	}
 }
 
@@ -29,14 +29,14 @@ void _eputs(char *leo_str)
 int _eputchar(char leo_c)
 {
 	static int leo_i;
-	static char buf[WRITE_BUF_SIZE];
+	static char leo_buf[WRITE_BUF_SIZE];
 
 	if (leo_c == BUF_FLUSH || leo_i >= WRITE_BUF_SIZE)
 	{
-		write(2, buf, leo_i);
+		write(2, leo_buf, leo_i);
 		leo_i = 0;
 	}
 	if (leo_c != BUF_FLUSH)
-		buf[leo_i++] = leo_c;
+		buf[leo_i = leo_i + 1] = leo_c;
 	return (1);
 }
